9bac089ee562affb61a5d84de86337e27bb6f6c0
isurupoorna <isuru.poorna28@gmail.com>
2020-12-20 09:39:32 +0530

    confic new project
    install bootstrap
    add admin model
     -> dashboard
     -> product

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,9 +1,23 @@
+import { AddProductComponent } from './add-product/add-product.component';
+import { ProductComponent } from './product/product.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminComponent } from './admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+
+const routes: Routes = [
+  { path: '',
+    component: AdminComponent,
+    children: [
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'products', component: ProductComponent},
+      {path: 'add-product', component: AddProductComponent}
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductComponent } from './product/product.component';
+import { AddProductComponent } from './add-product/add-product.component';
+
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, DashboardComponent, ProductComponent, AddProductComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
